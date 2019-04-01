f67ec86c2209ae043fc0e69e4854ac39311ef5c5
Nihal Singh <47137373+nihalsingh99@users.noreply.github.com>
2019-04-01 21:57:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title></title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="index.html" class="logo">BWS-HEATHCARE INDIA</a>
+					<nav id="nav">
+						<a href="index.html">Home</a>
+						<a href="company.html">The Company</a>
+						<a href="DD.html">Doctor's Desk</a>
+						<a href="Team.html">Our Team</a>
+						<a href="Products.html">Products</a>
+					</nav>
+				</div>
+			</header>
+			<a href="#menu" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<h1>Introducing: <span>Quality Medicines<br />
+					At Affodable Price</span></h1>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one">
+				<div class="inner">
+					<header>
+						<img style="right: 133px !important;  position: absolute;" src="images/1.jpg" alt="" />
+						<h2>What we do?</h2>
+					</header>
+					<p>We provide good medicine at your reasonalbe price.<br/>
+						 Our main motto is to privent every one from infectionc,diseases,and any illness.<br/>
+						  We are basically concerned about our customers health.<br/>
+						We bring good medicine at your price and try to satisfy your needs.</p>
+				</div>
+			</section>
+		<!-- Two -->
+			<section id="two">
+				<div class="inner">
+					<article>
+						<div class="content">
+							<header>
+								<h3>Our Vision</h3>
+							</header>
+							<div class="image fit">
+								<img src="images/15.jpg" alt="" />
+							</div>
+							<p>To continually create value and bring pride to our stakeholders, partners, customers and the community at large.By providing good medicine to all.</p>
+						</div>
+					</article>
+					<article class="alt">
+						<div class="content">
+							<header>
+								<h3>Our Values</h3>
+							</header>
+							<div class="image fit">
+								<img src="images/2.jpg" alt="" />
+							</div>
+							<p>Our team works according to the people demand and also provides good quality to the people.we are concerned with the customer health more then anything.we are responsible and can be trusted.this this our main motto.we develop a core value to the society and to the bussiness patners associated with us.</p>
+						</div>
+					</article>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three">
+				<div class="inner">
+					<article>
+						<div class="content">
+												<header>
+								<h3>Pldge for best outcome</h3>
+									<img style="    width: 280px;" src="images/3.jpg" alt="" />
+							</header>
+							<p>Dedicated team targeted towards business growth</p>
+						</div>
+					</article>
+					<article>
+						<div class="content">
+						 <header>
+								<h3>Convenience for all</h3>
+								<img src="images/5.jpg" alt="" />
+							</header>
+							<p>We consider best policies for with all our associates.</p>
+						</div>
+					</article>
+					<article>
+					<div class="content">
+							<header>
+								<h3>Breaking The growth barriers</h3>
+								<img src="images/4.jpg" alt="" />
+							</header>
+							<p>We strive to grow beyond leaps and bounds.</p>
+					</div>
+					</article>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<section id="footer">
+				<div class="inner">
+					<header>
+						<h2>Get in Touch</h2>
+					</header>
+					<form method="post" action="#">
+						<div class="field half first">
+							<label for="name">Name</label>
+							<input type="text" name="name" id="name" />
+						</div>
+						<div class="field half">
+							<label for="email">Email</label>
+							<input type="text" name="email" id="email" />
+						</div>
+						<div class="field">
+							<label for="message">Message</label>
+							<textarea name="message" id="message" rows="6"></textarea>
+						</div>
+						<ul class="actions">
+							<li><input type="submit" value="Send Message" class="alt" /></li>
+						</ul>
+					</form>
+				</div>
+			</section>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
